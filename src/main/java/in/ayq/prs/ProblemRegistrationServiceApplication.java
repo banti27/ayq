1421a88acb4fe2c7f4c1f8408f7b89f597833b40
Bal Vikash Sharma <balvikash.sharma@wissen.com>
2024-03-18 21:39:40 +0530

    AYQPRS-1001 Initial Setup

---
+++ src/main/java/in/ayq/prs/ProblemRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package in.ayq.prs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemRegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/ayq/prs/ProblemRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ayq.prs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
